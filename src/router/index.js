7a75e022302746421c2aceb412b1ab1b1a0e9b97
Valera <manson413@gmail.com>
2020-03-18 08:06:54 +0200

    initial commit

---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+const routes = [{
+    path: '/',
+    name: 'Home',
+    component: Home
+}, {
+    path: '/login',
+    name: 'login',
+    meta: { layout: 'empty' },
+    component: () =>
+        import ('../views/Login.vue')
+}, {
+    path: '/categories',
+    name: 'categories',
+    meta: { layout: 'main' },
+    component: () =>
+        import ('../views/Categories.vue')
+}]
+
+const router = new VueRouter({
+    routes
+})
+
+export default router
