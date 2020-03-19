312777ba292c475d441b71b9d48b689bad617c99
Valera <manson413@gmail.com>
2020-03-19 12:25:55 +0200

    test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
     <ul>
       <li><router-link to='/'>Home</router-link></li>
       <li><router-link to='/login'>Login</router-link></li>
-      <li><router-link to='/categories'>Categories</router-link></li>
+      <li><router-link to='/test'>Categories</router-link></li>
     </ul>
   </div>
 </template>
